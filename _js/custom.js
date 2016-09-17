51e580fc731920f0143bc3c9fd81a58fe89b7317
Krzysztof Len <len.krzysztof@gmail.com>
2016-09-17 10:13:20 +0200

    Upload site

---
+++ _js/custom.js
@@ -0,0 +1,11 @@
+$(window).load(function() {
+
+
+ //.parallax(xPosition, speedFactor, outerHeight) options:
+ //xPosition - Horizontal position of the element
+ //inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
+ //outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
+ $('#parallax').parallax("50%", 0.1);
+
+
+});

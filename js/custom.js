53e7813c0654e740bfe5bbbd1954bb993b2d60a2
Krzysiek <hakkai660@gmail.com>
2017-03-21 22:46:24 +0100

    refactor js and index

--- js/custom.js
+++ js/custom.js
@@ -1,23 +1,15 @@
-$(window).load(function() {
+$(document).ready(function() {
 
-  $('.timer').countTo();
- //.parallax(xPosition, speedFactor, outerHeight) options:
- //xPosition - Horizontal position of the element
- //inertia - speed to move relative to vertical scroll. Example: 0.1 is one tenth the speed of scrolling, 2 is twice the speed of scrolling
- //outerHeight (true/false) - Whether or not jQuery should use it's outerHeight option to determine when a section is in the viewport
- $('#parallax').parallax("50%", 0.1);
+    $('.timer').countTo();
 
- $('#menu').onePageNav({
- 	currentClass: 'active',
- 	scrollOffset: 30
+    $('#parallax').parallax("50%", 0.1);
 
- });
+    $('#menu').onePageNav({currentClass: 'active', scrollOffset: 30});
 
- $(".header h1").mouseenter(function() {
-   $(this).toggleClass("underline");
- })
- .mouseleave(function() {
- $(this).toggleClass("underline");
- });
+    $(".header h1").mouseenter(function() {
+        $(this).toggleClass("underline");
+    }).mouseleave(function() {
+        $(this).toggleClass("underline");
+    });
 
 });
